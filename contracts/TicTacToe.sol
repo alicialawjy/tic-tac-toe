361ef13c297d452cc6d7b09b91acadab8b36d6bb
alicialawjy <alicialawjy@gmail.com>
2022-06-05 12:47:20 +0100

    additional files

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,172 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.4.24;
+
+/**
+ * @title TicTacToe contract
+ **/
+contract TicTacToe {
+    address[2] public players;
+
+    /**
+     turn
+     1 - players[0]'s turn
+     2 - players[1]'s turn
+     */
+    uint public turn = 1;
+
+    /**
+     status
+     0 - ongoing
+     1 - players[0] won
+     2 - players[1] won
+     3 - draw
+     */
+    uint public status;
+
+    /**
+    board status
+     0    1    2
+     3    4    5
+     6    7    8
+     */
+    uint[9] private board;
+
+    /**
+    check status
+    
+     */
+     uint[][] public combinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+     uint public count = 0;
+
+
+    /**
+      * @dev Deploy the contract to create a new game
+      * @param opponent The address of player2
+      **/
+    constructor(address opponent) public {
+        require(msg.sender != opponent, "No self play");
+        players = [msg.sender, opponent];
+    }
+
+    /**
+      * @dev Check a, b, c in a line are the same
+      * _threeInALine doesn't check if a, b, c are in a line
+      * @param a position a
+      * @param b position b
+      * @param c position c
+      **/    
+    function _threeInALine(uint a, uint b, uint c) private view returns (bool){
+        /*Check if from same player and not 0.*/
+        if (board[a] == board[b] && board[b] == board[c] && board[a] != 0 ) {
+          return true;
+        }
+        else {
+          return false;
+        }
+    }
+
+    /**
+     * @dev get the status of the game
+     * @param pos the position the player places at
+     * @return the status of the game
+     */
+    function _getStatus(uint pos) private view returns (uint) {
+        /*Please complete the code here.*/
+        for(uint i=0; i<combinations.length; i++){
+          if (_threeInALine(combinations[i][0], combinations[i][1], combinations[i][2])) {
+            return board[combinations[i][0]];
+          }
+        }
+
+        if (count == 9) {
+          return 3;
+        }
+        
+        return 0;
+    }
+
+    /**
+     * @dev ensure the game is still ongoing before a player moving
+     * update the status of the game after a player moving
+     * @param pos the position the player places at
+     */
+    modifier _checkStatus(uint pos) {
+        /*Please complete the code here.*/
+        require(status==0);
+
+        _;
+
+        status = _getStatus(pos);
+
+    }
+
+    /**
+     * @dev check if it's msg.sender's turn
+     * @return true if it's msg.sender's turn otherwise false
+     */
+    function myTurn() public view returns (bool) {
+       /*Please complete the code here.*/
+       if (players[turn-1]==msg.sender) {
+         return true;
+       }
+       else {
+         return false;
+       }
+    }
+
+    /**
+     * @dev ensure it's a msg.sender's turn
+     * update the turn after a move
+     */
+    modifier _myTurn() {
+      /*Please complete the code here.*/
+      require(myTurn());
+
+      _;
+
+      /*Change turn*/
+      if (turn == 1) {
+        turn = 2;
+      }
+      else {
+        turn = 1;
+      }
+    }
+
+    /**
+     * @dev check a move is valid
+     * @param pos the position the player places at
+     * @return true if valid otherwise false
+     */
+    function validMove(uint pos) public view returns (bool) {
+      /*Please complete the code here.*/
+      return ((board[pos] == 0) && (pos>=0) && (pos<=8));
+    }
+
+    /**
+     * @dev ensure a move is valid
+     * @param pos the position the player places at
+     */
+    modifier _validMove(uint pos) {
+      /*Please complete the code here.*/
+      require (validMove(pos));
+      _;
+    }
+
+    /**
+     * @dev a player makes a move
+     * @param pos the position the player places at
+     */
+    function move(uint pos) public _validMove(pos) _checkStatus(pos) _myTurn {
+        board[pos] = turn;
+        count += 1;
+    }
+
+    /**
+     * @dev show the current board
+     * @return board
+     */
+    function showBoard() public view returns (uint[9]) {
+      return board;
+    }
+}
